83948903499c57a7775ca213feb02aa359346f0a
Christian Reiniger <creinig@creinig.de>
2023-07-08 10:15:10 +0200

    ruby: change tests to Minitest::Spec

--- ruby/test/codewars/math_expr_test.rb
+++ ruby/test/codewars/math_expr_test.rb
@@ -1,11 +1,10 @@
 require 'bundler/setup'
-require 'minitest'
+require 'minitest/autorun'
 
 require 'codewars/math_expr'
 
-class MathExpressionTest < Minitest::Test
-  def test_standards
-    tests = [
+describe :calc do
+    [
       ['1+1', 2],
       ['1 - 1', 0],
       ['1* 1', 1],
@@ -15,11 +14,11 @@ class MathExpressionTest < Minitest::Test
       ['2 /2+3 * 4.75- -6', 21.25],
       ['12* 123', 1476],
       ['2 / (2 + 3) * 4.33 - -6', 7.732]
-    ]
-
-    tests.each do |pair|
-      assert_equal pair[1], calc(pair[0])
+    ].each do |expr, expected|
+      describe "when called with #{expr}" do
+        it "must return #{expected}" do
+          _(calc(expr)).must_equal expected
+        end
+      end
     end
-  end
 end
-
--- ruby/test/codewars/the_lift_test.rb
+++ ruby/test/codewars/the_lift_test.rb
@@ -1,25 +1,22 @@
 require 'bundler/setup'
-require 'minitest'
+require 'minitest/autorun'
 
 require 'codewars/the_lift'
 
-class TheLiftTest < Minitest::Test
-  def test_standard
+describe :the_lift do
     # Floors:    G     1      2        3     4      5      6         Answers:
     tests = [
-            [ [ [],   [],    [5,5,5], [],   [],    [],    [] ],     [0, 2, 5, 0]          ],
-	    [ [ [],   [],    [1,1],   [],   [],    [],    [] ],     [0, 2, 1, 0]          ],
-	    [ [ [],   [3,],  [4,],    [],   [5,],  [],    [] ],     [0, 1, 2, 3, 4, 5, 0] ],
-	    [ [ [],   [0,],  [],      [],   [2,],  [3,],  [] ],     [0, 5, 4, 3, 2, 1, 0] ]]
-      
-    for queues, answer in tests do
-      assert_equal answer, the_lift(queues, 5)
+            [ [ [],   [],    [5,5,5], [],   [],    [],    [] ], 5,  [0, 2, 5, 0]          ],
+	    [ [ [],   [],    [1,1],   [],   [],    [],    [] ], 5,  [0, 2, 1, 0]          ],
+	    [ [ [],   [3,],  [4,],    [],   [5,],  [],    [] ], 5,  [0, 1, 2, 3, 4, 5, 0] ],
+	    [ [ [],   [0,],  [],      [],   [2,],  [3,],  [] ], 5,  [0, 5, 4, 3, 2, 1, 0] ],
+            [ [ [1, 1, 1, 1, 1], [0, 0]                      ], 3,  [0, 1, 0, 1, 0]       ] ]
+  
+    tests.each do |queues, capacity, answer| 
+      describe "when called on #{queues.inspect} with capacity #{capacity}" do
+	it "must move to these floors: #{answer.inspect}" do
+	  _(the_lift(queues, capacity)).must_equal answer
+	end
+      end
     end
-  end
-
-  def test_special1
-    queues = [[1, 1, 1, 1, 1], [0, 0]]
-    capacity = 3
-    assert_equal [0, 1, 0, 1, 0], the_lift(queues, capacity)
-  end
 end
